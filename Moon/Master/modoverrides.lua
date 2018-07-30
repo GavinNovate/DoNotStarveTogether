7390450a8a149ad173ba9e8ffefb4e9703fead0f
GavinNovate <gavin.novate@gmail.com>
2018-07-31 04:52:16 +0800

    update

--- Moon/Master/modoverrides.lua
+++ Moon/Master/modoverrides.lua
@@ -1,8 +1,9 @@
 return {
-  ["workshop-1301033176"]={ configuration_options={ LANG="auto" }, enabled=true },
-  ["workshop-347079953"]={
-    configuration_options={ DFV_Language="EN", DFV_MinimalMode="default" },
-    enabled=true 
+  ["workshop-356420397"]={ configuration_options={  }, enabled=true },
+  ["workshop-356930882"]={ configuration_options={ uses=100, uses2=100 }, enabled=true },
+  ["workshop-370373189"]={
+    configuration_options={ [""]=0, W_Grow="yes", b_seeds="yes", greenhouserecipe="easy" },
+    enabled=true
   },
   ["workshop-374550642"]={ configuration_options={ MAXSTACKSIZE=99 }, enabled=true },
   ["workshop-375850593"]={ configuration_options={  }, enabled=true },
@@ -14,11 +15,43 @@ return {
       SHAREMINIMAPPROGRESS=true,
       SHOWFIREICONS=true,
       SHOWPLAYERICONS=true,
-      SHOWPLAYERSOPTIONS=2 
+      SHOWPLAYERSOPTIONS=2
     },
-    enabled=true 
+    enabled=true
   },
-  ["workshop-396026892"]={ configuration_options={ OPT_DIFFICULTY=1 }, enabled=true },
-  ["workshop-458587300"]={ configuration_options={ Ownership=false, Travel_Cost=32 }, enabled=true },
-  ["workshop-501385076"]={ configuration_options={  }, enabled=true } 
+  ["workshop-382604105"]={ configuration_options={  }, enabled=true },
+  ["workshop-387716835"]={ configuration_options={ san=10 }, enabled=true },
+  ["workshop-396026892"]={ configuration_options={ OPT_DIFFICULTY=0 }, enabled=true },
+  ["workshop-444235588"]={
+    configuration_options={
+      deluxeEndoFirepitBurnRate=0.75,
+      deluxeFirepitBurnRate=0.75,
+      dropLoot="yes",
+      endoDropLoot="yes",
+      heatStarBurnRate=0.9,
+      heatStarDropLoot="yes",
+      iceStarBurnRate=0.9,
+      iceStarDropLoot="yes",
+      recipeCost="cheap",
+      starsSpawnHounds="no"
+    },
+    enabled=true
+  },
+  ["workshop-458587300"]={ configuration_options={ Ownership=false, Travel_Cost=128 }, enabled=true },
+  ["workshop-501385076"]={ configuration_options={  }, enabled=true },
+  ["workshop-623749604"]={
+    configuration_options={
+      Craft="Easy",
+      Destroyable="DestroyByAll",
+      FoodSpoilage=0,
+      Language="TCh",
+      Position="Center",
+      Slots=80
+    },
+    enabled=true
+  },
+  ["workshop-907007729"]={
+    configuration_options={ AMOUNTBONUS=2, COOKTIME=0.5, FRESHBONUS=0.3, MININGREDIENTS=2, RECIPE="EASY" },
+    enabled=true
+  }
 }
