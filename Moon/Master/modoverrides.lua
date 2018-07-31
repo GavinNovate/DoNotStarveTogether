0bf0107c793123571b580cba2e4c815d9dbc1fb0
GavinNovate <gavin.novate@gmail.com>
2018-07-31 20:01:55 +0800

    add 4 mods

--- Moon/Master/modoverrides.lua
+++ Moon/Master/modoverrides.lua
@@ -1,5 +1,9 @@
 return {
   ["workshop-356420397"]={ configuration_options={  }, enabled=true },
+  ["workshop-370373189"]={
+    configuration_options={ [""]=0, W_Grow="yes", b_seeds="yes", greenhouserecipe="easy" },
+    enabled=true
+  },
   ["workshop-374550642"]={ configuration_options={ MAXSTACKSIZE=99 }, enabled=true },
   ["workshop-375850593"]={ configuration_options={  }, enabled=true },
   ["workshop-378160973"]={
@@ -10,11 +14,27 @@ return {
       SHAREMINIMAPPROGRESS=true,
       SHOWFIREICONS=true,
       SHOWPLAYERICONS=true,
-      SHOWPLAYERSOPTIONS=2 
+      SHOWPLAYERSOPTIONS=2
     },
-    enabled=true 
+    enabled=true
   },
   ["workshop-382604105"]={ configuration_options={  }, enabled=true },
+  ["workshop-387716835"]={ configuration_options={ san=10 }, enabled=true },
+  ["workshop-444235588"]={
+    configuration_options={
+      deluxeEndoFirepitBurnRate=0.75,
+      deluxeFirepitBurnRate=0.75,
+      dropLoot="yes",
+      endoDropLoot="yes",
+      heatStarBurnRate=0.9,
+      heatStarDropLoot="yes",
+      iceStarBurnRate=0.9,
+      iceStarDropLoot="yes",
+      recipeCost="cheap",
+      starsSpawnHounds="no"
+    },
+    enabled=true
+  },
   ["workshop-458587300"]={ configuration_options={ Ownership=false, Travel_Cost=128 }, enabled=true },
   ["workshop-501385076"]={ configuration_options={  }, enabled=true },
   ["workshop-623749604"]={
@@ -24,8 +44,12 @@ return {
       FoodSpoilage=0,
       Language="TCh",
       Position="Center",
-      Slots=80 
+      Slots=80
     },
-    enabled=true 
-  } 
+    enabled=true
+  },
+  ["workshop-907007729"]={
+    configuration_options={ AMOUNTBONUS=2, COOKTIME=0.5, FRESHBONUS=0.3, MININGREDIENTS=2, RECIPE="EASY" },
+    enabled=true
+  }
 }
