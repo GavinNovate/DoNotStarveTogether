fb6399d0f9fe6c8954fa1d83285352ef3c757ca4
GavinNovate <gavin.novate@gmail.com>
2018-07-31 08:39:21 +0800

    remove Caves

--- Moon/Master/modoverrides.lua
+++ Moon/Master/modoverrides.lua
@@ -1,10 +1,5 @@
 return {
   ["workshop-356420397"]={ configuration_options={  }, enabled=true },
-  ["workshop-356930882"]={ configuration_options={ uses=100, uses2=100 }, enabled=true },
-  ["workshop-370373189"]={
-    configuration_options={ [""]=0, W_Grow="yes", b_seeds="yes", greenhouserecipe="easy" },
-    enabled=true
-  },
   ["workshop-374550642"]={ configuration_options={ MAXSTACKSIZE=99 }, enabled=true },
   ["workshop-375850593"]={ configuration_options={  }, enabled=true },
   ["workshop-378160973"]={
@@ -15,28 +10,11 @@ return {
       SHAREMINIMAPPROGRESS=true,
       SHOWFIREICONS=true,
       SHOWPLAYERICONS=true,
-      SHOWPLAYERSOPTIONS=2
+      SHOWPLAYERSOPTIONS=2 
     },
-    enabled=true
+    enabled=true 
   },
   ["workshop-382604105"]={ configuration_options={  }, enabled=true },
-  ["workshop-387716835"]={ configuration_options={ san=10 }, enabled=true },
-  ["workshop-396026892"]={ configuration_options={ OPT_DIFFICULTY=0 }, enabled=true },
-  ["workshop-444235588"]={
-    configuration_options={
-      deluxeEndoFirepitBurnRate=0.75,
-      deluxeFirepitBurnRate=0.75,
-      dropLoot="yes",
-      endoDropLoot="yes",
-      heatStarBurnRate=0.9,
-      heatStarDropLoot="yes",
-      iceStarBurnRate=0.9,
-      iceStarDropLoot="yes",
-      recipeCost="cheap",
-      starsSpawnHounds="no"
-    },
-    enabled=true
-  },
   ["workshop-458587300"]={ configuration_options={ Ownership=false, Travel_Cost=128 }, enabled=true },
   ["workshop-501385076"]={ configuration_options={  }, enabled=true },
   ["workshop-623749604"]={
@@ -46,12 +24,8 @@ return {
       FoodSpoilage=0,
       Language="TCh",
       Position="Center",
-      Slots=80
+      Slots=80 
     },
-    enabled=true
-  },
-  ["workshop-907007729"]={
-    configuration_options={ AMOUNTBONUS=2, COOKTIME=0.5, FRESHBONUS=0.3, MININGREDIENTS=2, RECIPE="EASY" },
-    enabled=true
-  }
+    enabled=true 
+  } 
 }
