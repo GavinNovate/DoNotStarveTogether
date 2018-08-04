8ba4e2b251cd59c7ca8117bb09e6fef85434aed9
GavinNovate <gavin.novate@gmail.com>
2018-08-04 23:22:51 +0800

    fix leveldataoverride and add mods

--- Moon/Caves/modoverrides.lua
+++ Moon/Caves/modoverrides.lua
@@ -1,8 +1,12 @@
 return {
+  ["workshop-1301033176"]={ configuration_options={ LANG="auto" }, enabled=true },
+  ["workshop-1448429176"]={ configuration_options={ JDKG=false, Language=false }, enabled=true },
+  ["workshop-350811795"]={ configuration_options={  }, enabled=true },
   ["workshop-356420397"]={ configuration_options={  }, enabled=true },
+  ["workshop-356930882"]={ configuration_options={ uses=100, uses2=100 }, enabled=true },
   ["workshop-370373189"]={
     configuration_options={ [""]=0, W_Grow="yes", b_seeds="yes", greenhouserecipe="easy" },
-    enabled=true
+    enabled=true 
   },
   ["workshop-374550642"]={ configuration_options={ MAXSTACKSIZE=99 }, enabled=true },
   ["workshop-375850593"]={ configuration_options={  }, enabled=true },
@@ -14,28 +18,71 @@ return {
       SHAREMINIMAPPROGRESS=true,
       SHOWFIREICONS=true,
       SHOWPLAYERICONS=true,
-      SHOWPLAYERSOPTIONS=2
+      SHOWPLAYERSOPTIONS=2 
     },
-    enabled=true
+    enabled=true 
   },
-  ["workshop-382604105"]={ configuration_options={  }, enabled=true },
-  ["workshop-387716835"]={ configuration_options={ san=10 }, enabled=true },
+  ["workshop-378962221"]={ configuration_options={  }, enabled=true },
+  ["workshop-380423963"]={
+    configuration_options={
+      [""]=0,
+      boulder_blue=0.4,
+      boulder_purple=0.1,
+      change_cave_loot=false,
+      common_loot_charcoal=0,
+      common_loot_flint=0.35,
+      common_loot_rocks=0.35,
+      cutlichen=0,
+      durian=0,
+      flintless_blue=0.5,
+      flintless_purple=0.15,
+      flintless_red=0.3,
+      foliage=0,
+      gears=0,
+      goldvein_purple=0.1,
+      goldvein_red=0.2,
+      guano=0,
+      ice=0,
+      lightbulb=0,
+      moon_green=0.05,
+      moon_orange=0.05,
+      moon_yellow=0.05,
+      pinecone=0,
+      rare_loot_bluegem=0.015,
+      rare_loot_marble=0.015,
+      rare_loot_redgem=0.015,
+      rottenegg=0,
+      seeds=0,
+      spoiled_food=0,
+      stalagmite_green=0.05,
+      stalagmite_orange=0.05,
+      stalagmite_yellow=0.05,
+      uncommon_loot_goldnugget=0.05,
+      uncommon_loot_mole=0.05,
+      uncommon_loot_nitre=0.05,
+      uncommon_loot_rabbit=0.05 
+    },
+    enabled=true 
+  },
   ["workshop-444235588"]={
     configuration_options={
-      deluxeEndoFirepitBurnRate=0.75,
-      deluxeFirepitBurnRate=0.75,
+      deluxeEndoFirepitBurnRate=0.5,
+      deluxeFirepitBurnRate=0.5,
       dropLoot="yes",
       endoDropLoot="yes",
-      heatStarBurnRate=0.9,
+      heatStarBurnRate=0.72,
       heatStarDropLoot="yes",
-      iceStarBurnRate=0.9,
+      iceStarBurnRate=0.72,
       iceStarDropLoot="yes",
       recipeCost="cheap",
-      starsSpawnHounds="no"
+      starsSpawnHounds="no" 
     },
-    enabled=true
+    enabled=true 
   },
+  ["workshop-456028339"]={ configuration_options={  }, enabled=true },
   ["workshop-458587300"]={ configuration_options={ Ownership=false, Travel_Cost=128 }, enabled=true },
+  ["workshop-466732225"]={ configuration_options={  }, enabled=true },
+  ["workshop-478005098"]={ configuration_options={  }, enabled=true },
   ["workshop-501385076"]={ configuration_options={  }, enabled=true },
   ["workshop-623749604"]={
     configuration_options={
@@ -44,12 +91,25 @@ return {
       FoodSpoilage=0,
       Language="TCh",
       Position="Center",
-      Slots=80
+      Slots=80 
     },
-    enabled=true
+    enabled=true 
+  },
+  ["workshop-728459184"]={
+    configuration_options={
+      INCREASEBACKPACKSIZES_BACKPACK=14,
+      INCREASEBACKPACKSIZES_ICEPACK=12,
+      INCREASEBACKPACKSIZES_KRAMPUSSACK=18,
+      INCREASEBACKPACKSIZES_PIGGYBACK=16,
+      largerchester=9,
+      largerdragonflychest=24,
+      largericebox=16,
+      largertreasurechest=16 
+    },
+    enabled=true 
   },
   ["workshop-907007729"]={
     configuration_options={ AMOUNTBONUS=2, COOKTIME=0.5, FRESHBONUS=0.3, MININGREDIENTS=2, RECIPE="EASY" },
-    enabled=true
-  }
+    enabled=true 
+  } 
 }
